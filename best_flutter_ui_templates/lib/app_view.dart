ca38eb7ae878f93058592fc681bb8d939cacfe23
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-04-20 17:32:34 +0700

    Thay đổi logo cho App thực hiện dời đăng nhập vào pj khoản 30%

---
+++ best_flutter_ui_templates/lib/app_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:best_flutter_ui_templates/blocs/my_user_bloc/my_user_bloc.dart';
+import 'package:best_flutter_ui_templates/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:best_flutter_ui_templates/blocs/update_user_info_bloc/update_user_info_bloc.dart';
+import 'package:best_flutter_ui_templates/auth/welcome_screen.dart';
+
+import 'blocs/authentication_bloc/authentication_bloc.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+			debugShowCheckedModeBanner: false,
+			title: 'best_flutter_ui_templates',
+			theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Color.fromARGB(255, 221, 220, 220),
+          onBackground: Colors.black,
+          primary: Color.fromRGBO(206, 147, 216, 1),
+          onPrimary: Colors.black,
+          secondary: Color.fromRGBO(244, 143, 177, 1),
+          onSecondary: Colors.white,
+					tertiary: Color.fromRGBO(255, 204, 128, 1),
+          error: Colors.red,
+					outline: Color(0xFF424242)
+        ),
+			),
+			home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+				builder: (context, state) {
+					if(state.status == AuthenticationStatus.authenticated) {
+						return MultiBlocProvider(
+								providers: [
+									BlocProvider(
+										create: (context) => SignInBloc(
+											userRepository: context.read<AuthenticationBloc>().userRepository
+										),
+									),
+									BlocProvider(
+										create: (context) => UpdateUserInfoBloc(
+											userRepository: context.read<AuthenticationBloc>().userRepository
+										),
+									),
+									BlocProvider(
+										create: (context) => MyUserBloc(
+											myUserRepository: context.read<AuthenticationBloc>().userRepository
+										)..add(GetMyUser(
+											myUserId: context.read<AuthenticationBloc>().state.user!.uid
+										)),
+									),
+									BlocProvider(
+										create: (context) => GetPostBloc(
+											postRepository: FirebasePostRepository()
+										)..add(GetPosts())
+									)
+								],
+							child: const HomeScreen(),
+						);
+					} else {
+						return const WelcomeScreen();
+					}
+				}
+			),
+		);
+  }
+}
