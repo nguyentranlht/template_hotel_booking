796da10e9454c5799179b1ab8e91107dfefd6ab3
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-05-04 23:56:14 +0700

    fix

--- best_flutter_ui_templates/lib/model/homelist.dart
+++ best_flutter_ui_templates/lib/model/homelist.dart
@@ -1,5 +1,4 @@
 import 'package:best_flutter_ui_templates/design_course/home_design_course.dart';
-import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
 import 'package:best_flutter_ui_templates/hotel_booking/hotel_home_screen.dart';
 import 'package:best_flutter_ui_templates/introduction_animation/introduction_animation_screen.dart';
 import 'package:flutter/widgets.dart';
@@ -21,10 +20,6 @@ class HomeList {
     HomeList(
       imagePath: 'assets/hotel/hotel_booking.png',
       navigateScreen: HotelHomeScreen(),
-    ),
-    HomeList(
-      imagePath: 'assets/fitness_app/fitness_app.png',
-      navigateScreen: FitnessAppHomeScreen(),
     ),
     HomeList(
       imagePath: 'assets/design_course/design_course.png',
